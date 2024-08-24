1ea06515bd3d4ba053fa01b4865cea2849a23cc1
DenysZelenskyi <den.zelenskiy@gmail.com>
2024-08-23 17:58:00 -0700

    add hw 6

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,10 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type Pages = { title: string; likes: number; accounts: string[]; status: 'close' | 'open'; details?: PagesDetails;};
+type PagesDetails = { createAt: Date; updateAt: Date }
+
+const page1: Pages = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,11 +16,13 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Pages = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
 }
 
-export {};
+export { };
+
+
